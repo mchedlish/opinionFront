3a7e9b3a6d39a4d323c95c1d0ade5e5806d0b7b5
mchedlish <mchedlish@yahoo.com>
2021-02-06 18:48:16 +0400

    CSS Adjastments

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import './Post.css'
 import SomeImage from '../assets/images/mainimage.jpg'
+import Clock from '../assets/icons/clock.png'
+import {Link} from 'react-router-dom'
+
 const Post = () => {
 const styles = {
     backgroundImage: "url("+SomeImage+")",
     objectFit:'cover',
     backgroundRepeat:"no-repeat",
-    width: '300px',
-    height:'200px',
-    
-
-}
+    width: '100%',
+    height:'240px'
+    }
 
     return (
         <div className='post'>
+        <Link style={{textDecoration:'none'}}>
             <div className='image'>
             <img src={SomeImage} style={styles}/>
             </div>
-            <p className='category'>sajhdhakssakd</p>
-            <p className='headline'>sdjakdjhaskhdjkasjdaskdhakshdkahkaj</p>
-            <p className='excerpt' ></p>
-        </div>
+            <p className='category'>პოლიტიკა</p>
+            <p className='headline'>ოპოზიცია თუ არ შევიდა პარლამენტში, ჩამოერთმევათ მანდატები</p>
+            <p className='excerpt' >ასდჯალსჯჯალსდჯლასჯლასჯადჯალსკჯდალსჯდალდჯლაჯლჯლკლადლჯ</p>
+            <div className='reading'><span><img className='clock' src={Clock}/><span className='minutes'>2</span></span> წუთის საკითხავი</div>
+        </Link>
+            </div>
     );
 };
 
--- src/components/Stories.js
+++ src/components/Stories.js
@@ -4,15 +4,12 @@ import Post from './Post'
 
 const Stories = (props) => {
     return (
-        <div className='col-lg-6 stories row'>
-        <div className='col-lg-6'>
+        <div className='stories row'>
+        <div className='col-lg-12'>
         <Post/>
         <Post/>
         </div>
-        <div className='col-lg-6'>
-        <Post/>
-        <Post/>
-            </div>
+        
             
         </div>
     );
